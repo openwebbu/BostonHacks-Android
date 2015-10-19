b4eca5e376a88bcda4f9d2a314f03edd0ab4115b
clairew <cdwang@bu.edu>
2015-10-18 21:20:45 -0400

    reverted mainapplication

--- app/src/main/java/sean/bostonhacks/MainApplication.java
+++ app/src/main/java/sean/bostonhacks/MainApplication.java
@@ -12,5 +12,7 @@ public class MainApplication extends Application {
     public void onCreate(){
         Parse.initialize(this, "EWm45pIZTDbnjwhUIl09dUM0aM9lygXaicaiHmvv", "fJSWol5CBlMNNw2pCWOjSKnYdx4jsi94R82lhDt7");
         ParseInstallation.getCurrentInstallation().saveInBackground();
+
     }
 }
+
