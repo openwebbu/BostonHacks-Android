b87864672f9a3be566bc4102e598dcd482cfe46a
Lawrence Luo <luolawrence1@gmail.com>
2015-10-20 04:13:19 -0400

    added necessary files

--- app/src/main/java/sean/bostonhacks/MainApplication.java
+++ app/src/main/java/sean/bostonhacks/MainApplication.java
@@ -1,9 +1,9 @@
 package sean.bostonhacks;
 
 import android.app.Application;
+
 import com.parse.Parse;
 import com.parse.ParseInstallation;
-import com.parse.ParseObject;
 
 /**
  * Created by Claire on 10/18/2015.
@@ -12,7 +12,6 @@ public class MainApplication extends Application {
     public void onCreate(){
         Parse.initialize(this, "EWm45pIZTDbnjwhUIl09dUM0aM9lygXaicaiHmvv", "fJSWol5CBlMNNw2pCWOjSKnYdx4jsi94R82lhDt7");
         ParseInstallation.getCurrentInstallation().saveInBackground();
-
     }
 }
 
