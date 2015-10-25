ecf0a9ef794e568151ffa4d0389c9406b13afbe4
Sean Zhang <puzzledsean@gmail.com>
2015-10-25 15:32:04 -0400

    functioning twitter and email intents on community

--- app/src/main/java/sean/bostonhacks/CustomListAdapterCommunity.java
+++ app/src/main/java/sean/bostonhacks/CustomListAdapterCommunity.java
@@ -2,16 +2,16 @@ package sean.bostonhacks;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.ListAdapter;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,7 +37,7 @@ public class CustomListAdapterCommunity  extends ArrayAdapter<Community> {
 
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder holder = null;
-        Community rowItem = getItem(position);
+        final Community rowItem = getItem(position);
 
         LayoutInflater mInflater = (LayoutInflater) context
                 .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
@@ -55,20 +55,48 @@ public class CustomListAdapterCommunity  extends ArrayAdapter<Community> {
         holder.name.setText(rowItem.getName());
         holder.role.setText(rowItem.getRole());
         holder.twitter.setText(rowItem.getTwitterHandle());
+//        holder.twitter.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_announcement_white_24dp,0,0,0);
         holder.email.setText(rowItem.getEmail());
 
         holder.twitter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //do something
-                notifyDataSetChanged();
+                Intent tweetIntent = new Intent(Intent.ACTION_VIEW);
+                tweetIntent.putExtra(Intent.EXTRA_TEXT, "@"+rowItem.getTwitterHandle() + " ");
+                tweetIntent.setType("text/plain");
+
+                PackageManager packManager = context.getPackageManager();
+                List<ResolveInfo> resolvedInfoList = packManager.queryIntentActivities(tweetIntent, PackageManager.MATCH_DEFAULT_ONLY);
+
+                boolean resolved = false;
+                for(ResolveInfo resolveInfo: resolvedInfoList){
+                    if(resolveInfo.activityInfo.packageName.startsWith("com.twitter.android")){
+                        tweetIntent.setClassName(
+                                resolveInfo.activityInfo.packageName,
+                                resolveInfo.activityInfo.name );
+                        resolved = true;
+                        break;
+                    }
+                }
+                if(resolved){
+                    context.startActivity(tweetIntent);
+                }else{
+                    Intent i = new Intent();
+                    i.putExtra(Intent.EXTRA_TEXT, "");
+                    i.setAction(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse("https://twitter.com/intent/tweet?text=@"+rowItem.getTwitterHandle()));
+                    context.startActivity(i);
+                }
             }
         });
         holder.email.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //do something
-                notifyDataSetChanged();
+                Intent i = new Intent(Intent.ACTION_SENDTO);
+                Uri data = Uri.parse("mailto:"+rowItem.getEmail());
+                i.setData(data);
+                context.startActivity(Intent.createChooser(i, "Send mail..."));
+
             }
         });
 
