8edfdf103da65c6d1b3bde5a4ae929ccb31226af
Sean Zhang <puzzledsean@gmail.com>
2015-10-28 01:37:47 -0400

    fixed the 0 vs 12 time difference because the calendar library is annoying

--- app/src/main/java/sean/bostonhacks/Schedule.java
+++ app/src/main/java/sean/bostonhacks/Schedule.java
@@ -2,6 +2,10 @@ package sean.bostonhacks;
 
 import android.util.Log;
 
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
@@ -40,18 +44,69 @@ public class Schedule {
         this.content = content;
     }
     public String getTimeStamp(){
-        String amPM = "";
+        String amPM = "AM";
         Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
-
         cal.setTime(timestamp);
-        
-        if(cal.get(Calendar.AM_PM) ==0){
-            amPM = "AM";
+//        Log.v("time", cal.get(cal.HOUR_OF_DAY) + "");
+        switch(cal.get(cal.HOUR_OF_DAY)){
+            case 0:
+                cal.set(Calendar.HOUR_OF_DAY,12);
+                break;
+            case 12:
+                amPM="PM";
+                break;
+            case 13:
+                cal.set(Calendar.HOUR_OF_DAY, 1);
+                amPM = "PM";
+                break;
+            case 14:
+                cal.set(Calendar.HOUR_OF_DAY, 2);
+                amPM = "PM";
+                break;
+            case 15:
+                cal.set(Calendar.HOUR_OF_DAY, 3);
+                amPM = "PM";
+                break;
+            case 16:
+                cal.set(Calendar.HOUR_OF_DAY, 4);
+                amPM = "PM";
+                break;
+            case 17:
+                cal.set(Calendar.HOUR_OF_DAY, 5);
+                amPM = "PM";
+                break;
+            case 18:
+                cal.set(Calendar.HOUR_OF_DAY, 6);
+                amPM = "PM";
+                break;
+            case 19:
+                cal.set(Calendar.HOUR_OF_DAY, 7);
+                amPM = "PM";
+                break;
+            case 20:
+                cal.set(Calendar.HOUR_OF_DAY, 8);
+                amPM = "PM";
+                break;
+            case 21:
+                cal.set(Calendar.HOUR_OF_DAY, 9);
+                amPM = "PM";
+                break;
+            case 22:
+                cal.set(Calendar.HOUR_OF_DAY, 10);
+                amPM = "PM";
+                break;
+            case 23:
+                cal.set(Calendar.HOUR_OF_DAY, 11);
+                amPM = "PM";
+                break;
+            case 24:
+                cal.set(Calendar.HOUR_OF_DAY, 12);
+                amPM = "PM";
+                break;
+
         }
-        else{
-            amPM = "PM";
-        }
-        String curTime = String.format("%d:%02d", cal.get(cal.HOUR),cal.get(cal.MINUTE));
+
+        String curTime = String.format("%d:%02d", cal.get(cal.HOUR_OF_DAY),cal.get(cal.MINUTE));
         return curTime + amPM;
     }
     public void setTimeStamp(Date createdAt){
